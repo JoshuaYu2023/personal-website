3503fb0bda5556338b486f52109c913888fe685e
Joshua Yu <jyu6@cnm.edu>
2023-11-21 12:30:23 -0700

    added react

---
+++ frontend/frontend/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwindcss-animated'),
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: [],
+  }
+}
+
+export default config
