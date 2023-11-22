b98f1cd5659a55135a265fafb219379b9aca9af1
Joshua Yu <jyu6@cnm.edu>
2023-11-22 15:08:21 -0700

    add padding to ProjectHero

--- frontend/src/app/components/ProjectHero.tsx
+++ frontend/src/app/components/ProjectHero.tsx
@@ -9,15 +9,16 @@ type ProjectHeroProps = {
 }
 
 
-
 export function ProjectHero({image, alt, link, title, description}: ProjectHeroProps) {
     return (
-        <div className="hero mx-auto w-full h-auto border-8 border-green-800">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={image} className="max-w-xl rounded-lg"  alt={alt}/>
-                <div>
-                    <Link href={link}><h1 className="text-5xl font-bold">{title}</h1></Link>
-                    <p className="py-6">{description}</p>
+        <div className={'p-6'}>
+            <div className={"hero mx-auto w-full h-auto border-8 border-green-800"}>
+                <div className={"hero-content flex-col lg:flex-row-reverse"}>
+                    <img src={image} className={"max-w-xl rounded-lg"} alt={alt}/>
+                    <div>
+                        <Link href={link}><h1 className={"text-5xl font-bold"}>{title}</h1></Link>
+                        <p className="py-6">{description}</p>
+                    </div>
                 </div>
             </div>
         </div>
