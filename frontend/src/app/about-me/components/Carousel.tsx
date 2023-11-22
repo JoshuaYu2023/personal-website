621cd11d92b7a55e1fa75f4b62d171528e11986e
Joshua Yu <jyu6@cnm.edu>
2023-11-22 12:47:31 -0700

    add Music and DND components

---
+++ frontend/src/app/about-me/components/Carousel.tsx
@@ -0,0 +1,53 @@
+export function Carousel() {
+    return (
+        <section className="md:p-6">
+            <h2 className="text-lg font-bold text-center p-4">MUSIC</h2>
+            <div className="carousel w-full">
+                <CarouselItem id={'item1'}
+                              spotify={"https://open.spotify.com/embed/playlist/7oiHCfqJHjtaq3JbJhRk90?utm_source=generator"}
+                              text={'This is my playlist that I\'m constantly editing, adding songs I like and removing others to create the perfect mood for me. It\'s made up of all sorts of genres, and never fails to reset my mood to get me out of any funk.'}/>
+                <CarouselItem id={'item2'}
+                              spotify={"https://open.spotify.com/embed/playlist/0uT4uy3DYMzs3h9wkhAbVY?utm_source=generator"}
+                              text={'My favorite instrumental artist is Samuel Kim. He takes certain soundtracks from film, TV, anime, and games amd turns them into Epic/Cinematic Arrangements.'}/>
+                <CarouselItem id={'item3'}
+                              spotify={"https://open.spotify.com/embed/track/71HkfiF7Y8eCymdkiVEk69?utm_source=generator"}
+                              text={'My favorite song has always changed over the years, but currently it is this song.'}/>
+            </div>
+            <CarouselNav/>
+        </section>
+    )
+}
+
+type CarouselItemProps = {
+    id: string
+    spotify: string
+    text: string
+}
+
+
+function CarouselItem({id, spotify, text}: CarouselItemProps) {
+    return (
+        <div id={id} className={"carousel-item w-full flex-col items-center text-center"}>
+            <iframe style={{borderRadius: '12px'}}
+                    src={spotify}
+                    width={"60%"}
+                    height={"152"}
+                    frameBorder={"0"}
+                    allow={"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"}
+                    loading={"lazy"}></iframe>
+            <div className={"container my-4 text-sm w-3/5"}>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+}
+
+function CarouselNav() {
+    return (
+        <div className={"flex justify-center w-full py-2 gap-2"}>
+            <a href={'#item1'} className={"btn btn-xs"}>1</a>
+            <a href={'#item2'} className={"btn btn-xs"}>2</a>
+            <a href={'#item3'} className={"btn btn-xs"}>3</a>
+        </div>
+    )
+}
--- frontend/src/app/about-me/page.tsx
+++ frontend/src/app/about-me/page.tsx
@@ -3,6 +3,8 @@
 import {Navbar} from "@/app/components/Navbar"
 import {Header} from "@/app/about-me/components/Header"
 import {AboutMeHero} from "@/app/about-me/components/AboutMeHero";
+import {Carousel} from "@/app/about-me/components/Carousel";
+import {DNDHero} from "@/app/about-me/components/DNDHero";
 
 
 export default function AboutMe() {
@@ -12,6 +14,10 @@ export default function AboutMe() {
                 <Navbar/>
                 <Header/>
                 <AboutMeHero/>
+                <div className={"container mx-auto my-4 md:flex gap-12 max-md:space-y-12"}>
+                    <Carousel/>
+                    <DNDHero/>
+                </div>
                 {/*TODO: copy over components from HTML doc. */}
             </div>
         </>
