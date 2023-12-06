3e14031bdadeb9b70477acf5199cdffe7afbc1a1
Joshua Yu <jyu6@cnm.edu>
2023-12-06 16:26:15 -0700

    add JS Events page

---
+++ frontend/src/app/js-events/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+import {Navbar} from "@/app/components/Navbar";
+
+export default function JavaScriptEvents() {
+    return (
+        <>
+            <Navbar/>
+
+        </>
+    )
+}
