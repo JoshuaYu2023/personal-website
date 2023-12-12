bea74b8d87c7e92105ef72e8a96031fca546db1f
Joshua Yu <jyu6@cnm.edu>
2023-12-12 14:47:13 -0700

    add styling to section

--- frontend/src/app/js-events/components/FindReplace.tsx
+++ frontend/src/app/js-events/components/FindReplace.tsx
@@ -2,19 +2,19 @@ import React, { useState } from "react"
 
 export function Content() {
     return(
-        <section>
-            <div className={'container mx-auto my-4'}>
+        <section className={'container border-2 border-green-800 mx-auto px-4'}>
+            <div className={'my-4'}>
                 <h1 className={'font-bold italic'}>Find and Replace</h1>
                 <p>Another thing you can do with JavaScript is take a sentence or paragraph and find specific words, replace them with something else, and see your new sentence! Let's do that below!</p>
             </div>
-            <div>
+            <div className={'my-4'}>
                 <FindAndReplace/>
             </div>
         </section>
     )
 }
 
-export function FindAndReplace () {
+function FindAndReplace () {
     const [text, setText] = useState(
         "This text will change"
     )
@@ -33,22 +33,25 @@ export function FindAndReplace () {
         setReplaceTerm(event.target.value)
     }
     return (
-        <div className={"container mx-auto"}>
+        <div className={"container mx-auto grid grid-cols-2"}>
             <textarea
                 onChange={handleTextChange}
                 placeholder={"Enter text here..."}
             />
-            <div className={"container mx-auto grid my-2"}>
-                <label htmlFor={"find-term"}>Find:</label>
+            <p className={"container text-center m-2 p-2 border-2 border-green-800"}>{text}</p>
+            <div className={"container mx-auto my-2 grid"}>
+                <label className={'mx-5'} htmlFor={"find-term"}>Find:</label>
                 <input
+                    className={'text-black'}
                     type={"text"}
                     id={"find-term"}
                     value={findTerm}
                     onChange={handleFindTermChange}
                     placeholder={"Enter word/phrase to find"}
                 />
-                <label htmlFor={"replace-term"}>Replace with:</label>
+                <label className={'mx-5'} htmlFor={"replace-term"}>Replace with:</label>
                 <input
+                    className={'text-black'}
                     type={"text"}
                     id={"replace-term"}
                     value={replaceTerm}
@@ -56,8 +59,7 @@ export function FindAndReplace () {
                     placeholder={"Enter replacement word/phrase"}
                 />
             </div>
-            <button className={'btn btn-outline'} onClick={handleFindAndReplace}>Find & Replace All</button>
-            <p className={"container text-center my-2 p-2 border-2 border-green-800"}>{text}</p>
+            <button className={'btn btn-outline m-2'} onClick={handleFindAndReplace}>Find & Replace All</button>
         </div>
     )
 }
