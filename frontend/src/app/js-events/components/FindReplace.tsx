1c39c03c0f41547ed663b09f5834258c340e1d5b
Joshua Yu <jyu6@cnm.edu>
2023-12-12 14:23:21 -0700

    make a working find and replace

--- frontend/src/app/js-events/components/FindReplace.tsx
+++ frontend/src/app/js-events/components/FindReplace.tsx
@@ -1,13 +1,63 @@
-export function FindAndReplace() {
+import React, { useState } from "react"
+
+export function Content() {
     return(
         <section>
-            <div className={'container mx-auto my-4 md:flex gap-12 max-md:space-y-12'}>
-                <h1>Find and Replace</h1>
+            <div className={'container mx-auto my-4'}>
+                <h1 className={'font-bold italic'}>Find and Replace</h1>
                 <p>Another thing you can do with JavaScript is take a sentence or paragraph and find specific words, replace them with something else, and see your new sentence! Let's do that below!</p>
             </div>
             <div>
-
+                <FindAndReplace/>
             </div>
         </section>
+    )
+}
+
+export function FindAndReplace () {
+    const [text, setText] = useState(
+        "This text will change"
+    )
+    const [findTerm, setFindTerm] = useState("")
+    const [replaceTerm, setReplaceTerm] = useState("")
+    const handleFindAndReplace = () => {
+        setText(text.replaceAll(findTerm, replaceTerm))
+    }
+    const handleTextChange = (event: any) => {
+        setText(event.target.value)
+    }
+    const handleFindTermChange = (event: any) => {
+        setFindTerm(event.target.value)
+    }
+    const handleReplaceTermChange = (event: any) => {
+        setReplaceTerm(event.target.value)
+    }
+    return (
+        <div className={"container mx-auto"}>
+            <textarea
+                onChange={handleTextChange}
+                placeholder={"Enter text here..."}
+            />
+            <div className={"container mx-auto grid my-2"}>
+                <label htmlFor={"find-term"}>Find:</label>
+                <input
+                    type={"text"}
+                    id={"find-term"}
+                    value={findTerm}
+                    onChange={handleFindTermChange}
+                    placeholder={"Enter word/phrase to find"}
+                />
+                <label htmlFor={"replace-term"}>Replace with:</label>
+                <input
+                    type={"text"}
+                    id={"replace-term"}
+                    value={replaceTerm}
+                    onChange={handleReplaceTermChange}
+                    placeholder={"Enter replacement word/phrase"}
+                />
+            </div>
+            <button className={'btn btn-outline'} onClick={handleFindAndReplace}>Find & Replace All</button>
+            <p className={"container text-center my-2 p-2 border-2 border-green-800"}>{text}</p>
+        </div>
     )
 }
--- frontend/src/app/js-events/page.tsx
+++ frontend/src/app/js-events/page.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import {Navbar} from "@/app/components/Navbar"
-import {Header} from "@/app/components/Header";
-import {Footer} from "@/app/components/Footer";
+import {Header} from "@/app/components/Header"
+import {Footer} from "@/app/components/Footer"
+import {Content} from "@/app/js-events/components/FindReplace";
+
 
 
 export default function JavaScriptEvents() {
     return (
         <>
             <Navbar/>
-            <Header text={'JavaScript is the part of code that makes a website do something. Every button and link that you click has a JavaScript code snippet attached to it to make it work. Everything beyond text and color is JavaScript. On this page, I\'ll display some of the things you can do with JavaScript'}/>
-
+            <Header
+                text={'JavaScript is the part of code that makes a website do something. Every button and link that you click has a JavaScript code snippet attached to it to make it work. Everything beyond text and color is JavaScript. On this page, I\'ll display some of the things you can do with JavaScript'}/>
+            <Content/>
             <Footer/>
         </>
     )
