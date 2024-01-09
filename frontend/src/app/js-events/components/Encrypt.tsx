a82344db4faeb9621d65de645af7345bd99e66f9
Joshua Yu <jyu6@cnm.edu>
2024-01-09 13:46:29 -0700

    make a working ROT13 encryption

--- frontend/src/app/js-events/components/Encrypt.tsx
+++ frontend/src/app/js-events/components/Encrypt.tsx
@@ -1,7 +1,18 @@
 export function Encrypt() {
     return (
         <div>
-            <p className={"container text-center m-2 p-2 border-2 border-secondary rounded-lg"} id={"text"}></p>
+            <p className={'container text-center m-2 p-2 border-2 border-secondary rounded-lg'} id={'words'}>Life before death, strength before weakness, journey before destination. I will unite instead of divide. I will bring men together. I will take responsibility for what I have done. If I must fall, I will rise each time a better man.</p>
+            <button className={'btn btn-xs sm:btn-sm md:btn-md btn-primary m-2'} onClick={HandleEncryption}>Encrypt</button>
         </div>
     )
+}
+
+function HandleEncryption() {
+    const textElement = document.getElementById('words') as HTMLParagraphElement
+    const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    textElement.innerText = textElement.innerText.replace(/[a-z]/gi, (letter) => {
+        const index = originalAlpha.indexOf(letter)
+        return index !== -1 ? cipher[index] : letter
+    })
 }
