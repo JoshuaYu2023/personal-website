2e21ff5a44321fdf334dffa319aa0d9ef09dbd14
Joshua Yu <jyu6@cnm.edu>
2024-01-09 13:59:50 -0700

    remove unused code

--- frontend/src/app/js-events/components/Encrypt.tsx
+++ frontend/src/app/js-events/components/Encrypt.tsx
@@ -15,7 +15,6 @@ export function Encrypt() {
                 placeholder={"Enter text here..."}
             />
             <p className={"container text-center m-2 p-2 border-2 border-secondary rounded-lg"} id={'words'}>{text}</p>
-            {/*<p className={'container text-center m-2 p-2 border-2 border-secondary rounded-lg'} id={'words'}>He was running through mists again. Walls, people, buildings all faded. Nothing but dark, swirling mists. But the mists had never been his enemy. Life as a human being is about posturing and influence. "I am the one thing you can never kill. I am Hope."</p>*/}
             <button className={'btn btn-xs sm:btn-sm md:btn-md btn-primary m-2'} onClick={HandleEncryption}>Encrypt
             </button>
             <p>If you click the button again, it will reset the encryption, as the shift of 13 places is just back to
