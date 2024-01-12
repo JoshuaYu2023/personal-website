16644e33217d7e47dc7f02aeca53bbd92a618401
Joshua Yu <jyu6@cnm.edu>
2024-01-12 11:30:39 -0600

    update not-found page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,5 +1,12 @@
+import {Navbar} from "@/app/components/Navbar";
+
 export default function NotFound() {
     return (
-        <h1>This is not the page you are looking for</h1>
+        <>
+        <Navbar/>
+        <div className={'text-4xl text-center my-10'}>
+        <h1 className={'font-bold italic'}>This is not the page you are looking for. Please go back.</h1>
+        </div>
+        </>
     )
 }
