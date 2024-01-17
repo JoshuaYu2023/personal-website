46599fabb02807a6cf4d6f5765ee40529860db0a
Joshua Yu <jyu6@cnm.edu>
2024-01-17 13:35:46 -0700

    clean up about-me page

--- frontend/src/app/about-me/components/ContactForm.tsx
+++ frontend/src/app/about-me/components/ContactForm.tsx
@@ -1,17 +1,21 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export function ContactForm() {
     return (
         <section className={"md:p-6"}>
-            <h2 className={"text-lg font-bold p-4"}>Contact Me! I would love to hear from you!</h2>
+            <div className={"py-8 px-4 mx-auto max-w-screen-md"}>
+                <h2 className={"text-lg font-bold p-4"}>Contact Me! I would love to hear from you!</h2>
+            </div>
             <div className={"grid grid-cols-2 justify-items-center"}>
                 <Links link={'https://www.linkedin.com/in/joshayu23/'} pic={'images/linked-in.jpg'}
                        alt={'LinkedIn Link'}/>
                 <Links link={'https://github.com/JoshuaYu2023'} pic={'images/github.jpg'} alt={'GitHub Link'}/>
             </div>
-                <div className={"py-8 px-4 mx-auto max-w-screen-md"}>
-                    <p className={'text-sm'}>Please reach out to me at <a className={'link text-green-800'} href={'mailto:joshayu17@outlook.com'}>joshayu17@outlook.com</a>. Thank you!</p>
-                </div>
+            <div className={"py-8 px-4 mx-auto max-w-screen-md"}>
+                <p className={'p-4 bg-white bg-opacity-10 rounded-lg'}>Please reach out to me at <a
+                    className={'link text-green-600'} href={'mailto:joshayu17@outlook.com'}>joshayu17@outlook.com</a>.
+                    Thank you!</p>
+            </div>
         </section>
     )
 }
@@ -26,7 +30,8 @@ type LinkProps = {
 
 function Links({link, pic, alt}: LinkProps) {
     return (
-        <Link href={link} target={"_blank"} rel={"noreferrer noopener"} className={'w-24 h-24 '}><img src={pic} alt={alt}
-                                                                             className={"rounded-full"}/></Link>
+        <Link href={link} target={"_blank"} rel={"noreferrer noopener"} className={'w-24 h-24 '}><img src={pic}
+                                                                                                      alt={alt}
+                                                                                                      className={"rounded-full"}/></Link>
     )
 }
